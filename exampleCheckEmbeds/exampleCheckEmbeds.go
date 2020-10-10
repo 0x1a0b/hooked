abdb1f52186b129f02ef28d1b87e0538edf22ea4
0x1a0b <65621431+0x1a0b@users.noreply.github.com>
2020-10-10 20:56:11 +0200

    finish examples

--- exampleCheckEmbeds/exampleCheckEmbeds.go
+++ exampleCheckEmbeds/exampleCheckEmbeds.go
@@ -25,7 +25,7 @@ func Setup() (i *Instance) {
 
 const (
 	InitialMessage = "init"
-	FireMessage = "yess hello there"
+	FireMessage = "yess hello there embeds"
 )
 
 func (i *Instance) Run() () {
@@ -58,6 +58,31 @@ func (i *Instance) SetHook() (h discordSender.Hook) {
 		Content: i.Message,
 		AvatarUrl: "https://i.kym-cdn.com/photos/images/newsfeed/000/925/493/19f.jpg",
 		Username: "Kappa",
+		Embeds: []discordSender.Embed{
+			{
+				Title: "bla bla bla",
+				Url: "https://keeb.io/",
+				Color: 14521290,
+                Author: discordSender.Author{
+					Name: "dem bot",
+					IconUrl: "http://placekitten.com/g/200/300",
+					Url: "http://placekitten.com",
+				},
+				Thumbnail: discordSender.Thumbnail{
+					Url: "https://www.gdargaud.net/Antarctica/Life/RacingPenguin.jpg",
+				},
+				Fields: []discordSender.Field{
+					{
+						Name: "hello",
+						Value: "there",
+						Inline: true,
+					},
+				},
+				Footer: discordSender.Footer{
+					Text: "Footer",
+				},
+			},
+		},
 	}
 
 	return
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/0x1a0b/hooked/config"
+	"github.com/0x1a0b/hooked/exampleCheckEmbeds"
 	"github.com/0x1a0b/hooked/exampleCheckSimple"
 	"github.com/0x1a0b/hooked/geekhack_rss"
 	"github.com/0x1a0b/hooked/steam_economy"
@@ -11,16 +12,18 @@ import (
 )
 
 func init() {
-	log.SetLevel(log.DebugLevel)
+	log.SetLevel(config.GetLogLevel())
 	log.SetReportCaller(true)
 	config.Read()
-	example = exampleCheckSimple.Setup()
+	exampleSimple = exampleCheckSimple.Setup()
+	exampleEmbeds = exampleCheckEmbeds.Setup()
 	time.Sleep(1 * time.Second)
 	log.Debugf("initialized")
 }
 
 var (
-	example *exampleCheckSimple.Instance
+	exampleEmbeds *exampleCheckEmbeds.Instance
+	exampleSimple *exampleCheckSimple.Instance
 )
 
 func main() {
@@ -28,7 +31,10 @@ func main() {
 	c := cron.New()
 	c.AddFunc("@every 10m", func() { go geekhack_rss.Update() })
 	c.AddFunc("@every 10m", func() { go steam_economy.UpdateShop() })
-	c.AddFunc("@every 1m", func() { go example.Run() })
+	if log.GetLevel() == log.TraceLevel {
+		c.AddFunc("@every 1m", func() { go exampleSimple.Run() })
+		c.AddFunc("@every 1m", func() { go exampleEmbeds.Run() })
+	}
 	log.Debugf("starting cron in foreground")
 	c.Run()
 }
