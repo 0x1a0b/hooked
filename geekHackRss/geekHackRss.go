1cf1001722c81a8bed55ddecf1d42869942f470a
0x1a0b <65621431+0x1a0b@users.noreply.github.com>
2020-10-10 23:07:26 +0200

    rewrites geekhack to use new pattern

--- geekHackRss/geekHackRss.go
+++ geekHackRss/geekHackRss.go
@@ -1,1 +1,102 @@
 package geekHackRss
+
+import (
+	"github.com/0x1a0b/hooked/config"
+	"github.com/0x1a0b/hooked/discordSender"
+	"github.com/mmcdole/gofeed"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	// https://wiki.simplemachines.org/smf/XML_feeds
+	GeekHackUrl = "https://geekhack.org/index.php?action=.xml;type=rss;limit=8;sa=news;board=70.0"
+)
+
+type Instance struct {
+	LastItem string
+	sender *discordSender.Sender
+	parser *gofeed.Parser
+	logger *logrus.Logger
+}
+
+func Setup() (i *Instance) {
+
+	i = &Instance{
+		LastItem: "",
+	}
+
+	secret := config.GetConf().Hooks.GeekHackNews
+	i.sender = discordSender.New(secret)
+
+	i.logger = logrus.New()
+	i.logger.SetLevel(config.GetLogLevel())
+	i.logger.SetReportCaller(true)
+
+	i.parser = gofeed.NewParser()
+
+	return
+
+}
+
+func (i *Instance) Run() () {
+	i.logger.Debugf("starting run")
+
+	feed, err := i.parser.ParseURL(GeekHackUrl)
+	if err != nil {
+		i.logger.Errorf("error parsing rss: %v", err)
+		return
+	}
+
+	if i.LastItem == "" {
+		i.LastItem = feed.Items[0].Link
+		i.logger.Debugf("Resuming with empty HEAD, setting %v", i.LastItem)
+		return
+	} else if i.LastItem == feed.Items[0].Link {
+		i.logger.Debugf("same HEAD, doing nothing")
+	} else {
+		for index, item := range feed.Items {
+			if item.Link == i.LastItem {
+				i.LastItem = item.Link
+				i.logger.Debugf("delta ended with %v at %v", item.Link, index)
+				break
+			} else {
+				i.Fire(item)
+			}
+		}
+	}
+
+	i.logger.Debugf("ending run")
+	return
+}
+
+func (i *Instance) Fire(item *gofeed.Item) () {
+	hook := i.SetHook(item)
+	if err := i.sender.Send(hook); err != nil {
+		i.logger.WithField("hook", hook).Errorf("error firing hook: %v", err)
+	} else {
+		i.logger.WithField("hook", hook).Debugf("fired hook")
+	}
+	return
+}
+
+func (i *Instance) SetHook(item *gofeed.Item) (h discordSender.Hook) {
+
+	h = discordSender.Hook{
+		Content: "New Geekhack post: " + item.Title,
+		AvatarUrl: "https://wiki.geekhack.org/images/thumb/5/51/Cherry_MX_White_Plate_Mount_Switch.jpg/300px-Cherry_MX_White_Plate_Mount_Switch.jpg",
+		Username: "Geekhack",
+		Embeds: []discordSender.Embed{
+		{
+			Title: item.Title,
+			Description: item.Content,
+            Color: 3581519,
+            Url: item.Link,
+            Thumbnail: discordSender.Thumbnail{
+				Url: "https://cdn.mos.cms.futurecdn.net/qhWUafmdRWYFhFSyAiAFPh-970-80.jpg",
+			},
+		},
+		},
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/0x1a0b/hooked/config"
 	"github.com/0x1a0b/hooked/exampleCheckEmbeds"
 	"github.com/0x1a0b/hooked/exampleCheckSimple"
-	"github.com/0x1a0b/hooked/geekhack_rss"
+	"github.com/0x1a0b/hooked/geekHackRss"
 	"github.com/0x1a0b/hooked/steam_economy"
 	"github.com/robfig/cron/v3"
 	log "github.com/sirupsen/logrus"
@@ -17,11 +17,14 @@ func init() {
 	config.Read()
 	exampleSimple = exampleCheckSimple.Setup()
 	exampleEmbeds = exampleCheckEmbeds.Setup()
+	geekHack = geekHackRss.Setup()
+
 	time.Sleep(1 * time.Second)
 	log.Debugf("initialized")
 }
 
 var (
+	geekHack *geekHackRss.Instance
 	exampleEmbeds *exampleCheckEmbeds.Instance
 	exampleSimple *exampleCheckSimple.Instance
 )
@@ -29,7 +32,7 @@ var (
 func main() {
 	// https://godoc.org/github.com/robfig/cron
 	c := cron.New()
-	c.AddFunc("@every 10m", func() { go geekhack_rss.Update() })
+	c.AddFunc("@every 10m", func() { go geekHack.Run() })
 	c.AddFunc("@every 10m", func() { go steam_economy.UpdateShop() })
 	if log.GetLevel() == log.TraceLevel {
 		c.AddFunc("@every 1m", func() { go exampleSimple.Run() })
